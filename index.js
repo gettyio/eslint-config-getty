c72bec8063f606de695f42fc9b8ba79fc4ec8214
Pedro Nakibar <pedronakibar@gmail.com>
2016-12-07 17:34:19 -0200

    added flow support

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 module.exports = {
   "parser": "babel-eslint",
-  "extends": "airbnb",
+  "extends": [
+    "airbnb",
+    "plugin:flowtype/recommended"
+  ],
   "env": {
     "node": true,
     "es6": true,
@@ -14,6 +17,7 @@ module.exports = {
     "react/jsx-boolean-value": 0,
     "max-len": [2, 200],
     "no-underscore-dangle": ["error", { "allow": ["_id", "__v"] }],
+    "flowtype",
   },
   parserOptions: {
     ecmaFeatures: {
@@ -28,7 +32,30 @@ module.exports = {
       "no-shadow": 0,
       "dot-notation": 0,
       "no-underscore-dangle": 0,
-      "import/no-unresolved": 0
+      "import/no-unresolved": 0,
+      "flowtype/boolean-style": [ 2, "boolean" ],
+      "flowtype/define-flow-type": 1,
+      "flowtype/delimiter-dangle": [ 2, "never" ],
+      "flowtype/generic-spacing": [ 2, "never" ],
+      "flowtype/no-primitive-constructor-types": 2,
+      "flowtype/no-weak-types": 2,
+      "flowtype/object-type-delimiter": [ 2,  "comma" ],
+      "flowtype/require-parameter-type": 2,
+      "flowtype/require-return-type": [ 2,  "always", { "annotateUndefined": "never" } ],
+      "flowtype/require-valid-file-annotation": [ 2, "always", { "annotationStyle": "line" }],
+      "flowtype/semi": [ 2,  "always" ],
+      "flowtype/space-after-type-colon": [ 2,  "always" ],
+      "flowtype/space-before-generic-bracket": [ 2, "never" ],
+      "flowtype/space-before-type-colon": [ 2, "never" ],
+      "flowtype/type-id-match": [ 2, "^([A-Z][a-z0-9]+)+Type$" ],
+      "flowtype/union-intersection-spacing": [ 2, "always" ],
+      "flowtype/use-flow-type": 1,
+      "flowtype/valid-syntax": 1
+    }
+  },
+  "settings": {
+    "flowtype": {
+      "onlyFilesWithFlowAnnotation": false
     }
   }
 }
